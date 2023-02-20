6cfc363417c0d277579bfa957cea1f78084c4a45
Xafy <57871519+Xafy@users.noreply.github.com>
2023-02-20 08:28:32 +0200

    test deploy

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const User = require('./models/user')
 const ExpressError = require("./utils/ExpressError")
 
 const mongoose = require('mongoose');
-const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/mokhaiam'
+const dbUrl = process.env.DB_URL ;
 mongoose.set('strictQuery',false);
 mongoose.connect(dbUrl);
 
